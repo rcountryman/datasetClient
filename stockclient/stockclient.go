16029cdc29ce1538e44817351af4d4520bf87be3
Ryan Countryman <Ryan.Countryman@gmail.com>
2022-07-26 22:38:23 -0600

    More todos

--- stockclient/stockclient.go
+++ stockclient/stockclient.go
@@ -14,6 +14,8 @@ func (client *StockClient) InitStockClient() {
 	client.apiKey = os.Getenv("STOCK_API_KEY")
 }
 
+// TODO Unit tests to verify error handling
+// TODO Injection of http client to mock for testing
 func (client StockClient) Get(stock string) (stringResp string) {
 	if client.apiKey == "" {
 		return "Not authorized"
